b7d03d89ee381fcbfce7a4d7d5759bbf219b415d
MAHI SAXENA <mahikvrk1606@gmail.com>
2025-09-03 15:56:53 +0530

    Add files via upload

---
+++ EVEN OR ODD.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main ()
+{
+              int x;
+              printf("enter the value of x");
+              scanf("%d",&x);
+              if(x%2==0)
+              {
+                            printf("even");
+              }
+              else
+              {
+                            printf("odd");
+
+              }
+}
---
+++ temp. celsius to fahrenheit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+              int c,f;
+              printf("Enter the temperature in celsius");
+              scanf("%d",&c);
+              f=(c*9/5)+32;
+              printf("Temperature in Farenheit:%d",f);
+}
