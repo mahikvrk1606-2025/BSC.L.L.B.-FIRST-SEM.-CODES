3bd1f0d860734f55a3d9e43f6200485080fb8294
MAHI SAXENA <mahikvrk1606@gmail.com>
2025-10-29 16:19:43 +0530

    Add files via upload

---
+++ 2-d array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    int arr[rows][cols];
+    printf("\nEnter elements of the array:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\nThe 2D array is:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
